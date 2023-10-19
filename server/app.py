33202b399fa16c7931b09b522410561d7f7b54fc
RegisLewis <regisaslewis@gmail.com>
2023-10-18 18:37:03 -0700

    querying a database in flask

--- server/app.py
+++ server/app.py
@@ -16,5 +16,39 @@ db.init_app(app)
 
 # add views here 
 
+@app.route("/")
+def index():
+    response = make_response(
+        "<h1>Welcome to the pet directory!</h1>",
+        200
+    )
+    return response
+
+@app.route("/pets/<int:id>")
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        response_body = f"<p>{pet.name}: {pet.species}</p>"
+        response_status = 202
+    else:
+        response_body = f"<p>Pet {id} not found.</p>"
+        response_status = 404
+
+    response = make_response(response_body, response_status)
+    return response
+
+@app.route("/species/<string:species>")
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species = species.title()).all()
+
+    size = len(pets)
+    response_body = f"<h2>There {'is' if size == 1 else 'are'} {size} {species.lower()}{'.' if size == 1 else 's.'}</h2>"
+    for n in pets:
+        response_body += f"<p>{n.name}</p>"
+    response = make_response(response_body, 200)
+
+    return response
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
